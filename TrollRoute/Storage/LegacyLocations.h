75eba4a94e5402e7eaf4e1bc183f3411cf29dd36
dm2mymcszt-commits <irals@icloud.com>
2026-09-13 21:40:59 +0200

    Declare the migration discovery result nonnull

--- TrollRoute/Storage/LegacyLocations.h
+++ TrollRoute/Storage/LegacyLocations.h
@@ -1,4 +1,4 @@
 #import <Foundation/Foundation.h>
 
 // Discovers existing containers only. It never creates, modifies or deletes one.
-FOUNDATION_EXPORT NSDictionary<NSString *, id> *TRLegacyLocations(void);
+FOUNDATION_EXPORT NSDictionary<NSString *, id> * _Nonnull TRLegacyLocations(void);
